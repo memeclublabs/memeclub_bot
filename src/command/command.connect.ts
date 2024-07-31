8b9c97cd13b28a02f28938bd55e734961c7211b6
Andrew <andrew.sideby@gmail.com>
2024-07-31 20:57:18 +0800

    connect

--- src/command/command.connect.ts
+++ src/command/command.connect.ts
@@ -1,7 +1,8 @@
 import { Bot } from "grammy";
 import { MyContext } from "../global.types";
 import { tonConnectInfoKeyboard } from "../service/use.ton-connect";
-import { contactAdminWithError } from "../com.utils";
+import { contactAdminWithError, formatTonAddressStr } from "../com.utils";
+import { CHAIN } from "@tonconnect/sdk";
 
 export function bind_command_connect(bot: Bot<MyContext>) {
   bot.command("connect", async (ctx) => {
@@ -12,7 +13,18 @@ export function bind_command_connect(bot: Bot<MyContext>) {
       await contactAdminWithError(ctx);
       return;
     }
-    await tonConnectInfoKeyboard(ctx, chatId);
+    let { isConnected, connector } = await tonConnectInfoKeyboard(ctx, chatId);
+    console.info("wallet connection:", isConnected);
+    let wallet = connector?.wallet;
+    if (wallet) {
+      await ctx.reply(
+        `<b>💎 Wallet Connected!</b> \n
+Wallet: ${wallet?.device?.appName}
+Network: ${wallet!.account.chain === CHAIN.TESTNET ? "Testnet" : "Mainnet"}
+Address:\n${formatTonAddressStr(wallet?.account.address!)}`,
+        { parse_mode: "HTML" },
+      );
+    }
     console.info("command - /connect ]", ctx.from?.username, Date.now());
   });
 }
