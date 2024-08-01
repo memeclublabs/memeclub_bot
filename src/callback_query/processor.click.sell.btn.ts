e477968d57e2f9bb4d91fbdd69b9d1f6365ff704
Andrew <andrew.sideby@gmail.com>
2024-08-01 16:28:49 +0800

    send Sell Sign Btn to user

--- src/callback_query/processor.click.sell.btn.ts
+++ src/callback_query/processor.click.sell.btn.ts
@@ -1,6 +1,7 @@
 import { MyContext } from "../global.types";
-import { contactAdminWithError } from "../com.utils";
-import { handlerClickSellBtn } from "./handler.click.sell.btn";
+import { buildMemecoinInfoText, contactAdminWithError } from "../com.utils";
+import prisma from "../prisma";
+import { InlineKeyboard } from "grammy";
 
 export async function processorClickSellBtn(
   ctx: MyContext,
@@ -10,4 +11,56 @@ export async function processorClickSellBtn(
     await contactAdminWithError(ctx, memecoinId);
   }
   await handlerClickSellBtn(ctx, Number(memecoinId));
+}
+
+export async function handlerClickSellBtn(ctx: MyContext, memecoinId: number) {
+  let findMeme = await prisma.memecoin.findUnique({
+    where: { id: memecoinId },
+  });
+  if (!findMeme) {
+    await contactAdminWithError(ctx, ` memecoin ${memecoinId} not found`);
+    return;
+  }
+
+  let findGroup = await prisma.group.findUnique({
+    where: { groupId: Number(findMeme.groupId) },
+  });
+  if (!findGroup) {
+    await contactAdminWithError(ctx, `group ${findMeme.groupId} not found`);
+    return;
+  }
+
+  let inlineKeyboard = new InlineKeyboard()
+    .text(
+      "25%",
+      JSON.stringify({
+        method: "clickSellWithPercentage",
+        data: `${findMeme.id}###25`,
+      }),
+    )
+    .text(
+      "50%",
+      JSON.stringify({
+        method: "clickSellWithPercentage",
+        data: `${findMeme.id}###50`,
+      }),
+    )
+    .row()
+    .text(
+      "75%",
+      JSON.stringify({
+        method: "clickSellWithPercentage",
+        data: `${findMeme.id}###75`,
+      }),
+    )
+    .text(
+      "100%",
+      JSON.stringify({
+        method: "clickSellWithPercentage",
+        data: `${findMeme.id}###100`,
+      }),
+    );
+
+  let text = buildMemecoinInfoText(findMeme, findGroup, "🔴 Sell Memecoin");
+  await ctx.reply(text, { parse_mode: "HTML", reply_markup: inlineKeyboard });
 }
